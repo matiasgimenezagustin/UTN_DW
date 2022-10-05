5a952163b9d628672daca97236faad393fcd505a
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-10-04 22:13:12 -0300

    clase 11 condicionales

---
+++ clase11/script.js
@@ -0,0 +1,81 @@
+/* index = indice */
+
+/* let lista = [1, 2, 3];
+
+console.log(lista[0]);
+
+
+let respuesta = prompt("hola")
+
+console.log(respuesta) */
+
+//Actividad 1
+
+/* let numero = parseFloat(prompt("ingrese un numero"))
+
+
+if(numero > 1000){
+    alert("es mayor a 1000")
+}else{
+    alert("es menor a 1000")
+} */
+
+/* condicion ? true : false */
+
+/* numero >= 1000 ?  alert("es mayor a 1000") : alert("es menor a 1000")
+ */
+
+//Actividad 2 
+/* 
+let mensaje = prompt("ingrese algo") */
+
+
+//Actividad 3
+
+/* let numero = parseFloat(prompt("ingrese un numero"))
+
+if(numero >= 10 && numero <= 50){
+    alert("El numero esta entre 10 y 50")
+}else if( numero < 10 || numero > 50){
+    alert("El numero es menor a 10 o mayor a 50")
+}else{
+    alert("ERROR: Ingresaste un valor no valido")
+} */
+//OR = ||
+// true true = true
+// true false/false true = true
+// false false = false
+
+//AND = &&
+// true true = true
+// true false/false true = false
+// false false = false
+
+/* if(mensaje === "Hola" || mensaje === "hola"){
+    document.write("Hola... buenos dias!")
+} */
+
+//Switch
+
+/* s */
+
+
+//ultima actividad
+/* let numero = parseFloat(prompt("Ingrese un numero"))
+
+let esMayorADiez = numero > 10 // puede solo ser TRUE o FALSE
+let esMenorOIgualADiez = numero <= 10
+
+if(esMayorADiez){
+
+    document.write("El numero es mayor a 10")
+
+}else if(esMenorOIgualADiez){
+
+    document.write("El numero es menor o igual a 10")
+
+}else{
+    
+    document.write("El dato ingresado no es un numero")
+    
+} */
